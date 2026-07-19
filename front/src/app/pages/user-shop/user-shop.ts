9dbeaa027e675a0991716e1603fbe270cb1fc376
Nathan Rous <ndrous2084@gmail.com>
2026-07-18 22:24:06 -0400

    much cleaner approach; css only - no typescript

--- front/src/app/pages/user-shop/user-shop.ts
+++ front/src/app/pages/user-shop/user-shop.ts
@@ -13,10 +13,18 @@ import { UserShopService } from '../../services/user-shop-service';
 })
 export class UserShop {
 
+  //SCROLLING
+  left_scroll = document.querySelector(".left");
+  right_scroll = document.querySelector(".right");
+
+
+  //SERVICES
   index = inject(UserIndexService);
   userShopService = inject(UserShopService);
-  readonly itemList = this.userShopService.items();
 
+
+  //EXTERNAL VARIABLES
+  readonly itemList = this.userShopService.items();
   private dialog = inject(Dialog)
 
   addItem(){
@@ -37,9 +45,22 @@ export class UserShop {
   })
   };
 
+
   removeItem(){};
   editItem(){};
   viewShop(){};
+
+  //CAROUSEL FUNCITONALITY
+
+  slideIndex: number = 1;
+  slide = document.querySelectorAll("slide-container");
+
+  scrollLeft(n: number){
+    console.log(this.slide);
+  };
+  scrollRight(n: number){
+    this.right_scroll?.scrollTo({left: -325, behavior: 'smooth'})
+  };
 
 
 }
