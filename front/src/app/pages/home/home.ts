ba5fd6b3d40c9bdc64e3fbed7df0fa90157eb18a
Nathan Rous <ndrous2084@gmail.com>
2026-07-20 21:42:11 -0400

    cateogry section added

--- front/src/app/pages/home/home.ts
+++ front/src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { Header } from './../../page-components/header/header';
 import { Router } from '@angular/router';
 import { UserIndexService } from '../../services/user-index-service';
 import { HeroSection } from "./hero-section/hero-section";
+import { CategorySection } from "./category-section/category-section";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, HeroSection],
+  imports: [Header, HeroSection, CategorySection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
