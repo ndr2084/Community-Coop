9f5a6fe4086836dfb65c108d0bd0f001aff0e0a2
Nathan Rous <ndrous2084@gmail.com>
2026-07-26 18:35:57 -0400

    latest stable

--- front/src/app/pages/home/category-section/category-section.ts
+++ front/src/app/pages/home/category-section/category-section.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-category-section',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './category-section.html',
   styleUrl: './category-section.css',
 })
 export class CategorySection {
+
+  searchCategory = new FormGroup({
+    category : new FormControl('')
+  });
 
 }
